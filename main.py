f8a6c3917b1a88809080dd080eae8d7c8ea3a69b
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-12-19 10:56:32 +0500

    Написал бота

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from datetime import datetime, date, time
 import time as timers
 
-weekdays = {1: 'Mon', 2: 'Tue', 3: 'Wed',
-            4: 'Thu', 5: 'Fri', 6: 'Sat', 7: 'Sun'}
+weekdays = {1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat', 7: 'Sun'}
 
 
 def create_times():
